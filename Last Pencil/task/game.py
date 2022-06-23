fec0530805cff62593f06d2e8b2f8f17ffb6921c
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-23 14:51:58 +0300

    Stage 3/5: Working on the gameplay

--- Last Pencil/task/game.py
+++ Last Pencil/task/game.py
@@ -2,15 +2,27 @@
 MSG_YOUR_TURN = "Your turn!"
 MSG_PENCILS = "How many pencils would you like to use:"
 MSG_PLAYER_ORDER = "Who will be the first"
+MSG_TURN = "turn:"
 
-name1, name2 = "Player 1", "Player 2"
+name1, name2 = "John", "Jack"
 
-# turn = input()
+
+def switch_turns(p):
+    if p == name1:
+        return name2
+    return name1
+
+
 print(MSG_PENCILS)
-pencils_count = int(input())
-pencils = "|" * pencils_count
-
+total_pencils = int(input())
+current_pencils = total_pencils
 print(f'{MSG_PLAYER_ORDER} ({name1}, {name2}):')
 name = input()
-print(pencils)
-print(f'{name} is going first!')
+while current_pencils > 0:
+    if current_pencils < total_pencils:
+        name = switch_turns(name)
+    pencils = "|" * current_pencils
+    print(pencils)
+    print(f"{name}'s {MSG_TURN}")
+    pencils_per_turn = int(input())
+    current_pencils -= pencils_per_turn
