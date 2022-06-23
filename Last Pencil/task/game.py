df0455e07c6f2d875c543eff9bd4cc1a4abec507
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-23 14:22:19 +0300

    Stage 2/5: New rules

--- Last Pencil/task/game.py
+++ Last Pencil/task/game.py
@@ -1,7 +1,16 @@
 
 MSG_YOUR_TURN = "Your turn!"
+MSG_PENCILS = "How many pencils would you like to use:"
+MSG_PLAYER_ORDER = "Who will be the first"
+
+name1, name2 = "Player 1", "Player 2"
 
 # turn = input()
-turn = "||||||"
-print(turn)
-print(MSG_YOUR_TURN)
+print(MSG_PENCILS)
+pencils_count = int(input())
+pencils = "|" * pencils_count
+
+print(f'{MSG_PLAYER_ORDER} ({name1}, {name2}):')
+name = input()
+print(pencils)
+print(f'{name} is going first!')
